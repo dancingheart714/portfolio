699763e77f4750f9549d31ee55f3f02e7626330a
Leah King <dancingheart714@yahoo.com>
2021-10-03 08:58:33 -0400

    modified some css

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal';
 
 class About extends Component {
   render() {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic= "images/" + this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
@@ -17,10 +18,14 @@ class About extends Component {
     }
 
     return (
-      <section id="about">
-      <div className="row">
+       <section id="about">
+          <Fade duration={1000}>
+            <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Leah King Profile Pic" />
+            <img 
+            className="profile-pic"  
+            src={profilepic} 
+            alt="Leah King Profile Pic"/>
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -30,25 +35,30 @@ class About extends Component {
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
+						   <span>{name}</span>
+                     <br />
+						   <span>{street}
+                     <br />
 						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
+                   </span>
+                   <br />
+						   <span>{phone}</span>
+                  <br />
                      <span>{email}</span>
 					   </p>
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={resumeDownload} className="button">
+                     <i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
          </div>
       </div>
-
+   </Fade>
    </section>
-    );
+   );
   }
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,61 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ParticlesBg from "particles-bg";
-import Fade from "react-reveal";
 
 class Header extends Component {
   render() {
+    if (!this.props.data) return null;
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var state= this.props.data.address.state;
-      var networks= this.props.data.social.map(function(network){
+    const github = this.props.data.github;
+    const name = this.props.data.name;
+    const description = this.props.data.description;
+    const state = this.props.data.state;
+    const occupation = this.props.data.occupation;
+     var networks = this.props.data.social.map(function (network) {
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
+     })
 
     return (
-       <header id="home">
-          <ParticlesBg type="circle" bg={true} />
+      <header id="home">
+        <ParticlesBg type="circle" bg={true} />
 
-      <nav id="nav-wrap">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
 
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
 
-      </nav>
-      
-      <div className="row banner">
-             <div className="banner-text">
-                <Fade bottom>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Projects
+              </a>
+            </li>
                 
+            <li>
+               <a className="smoothscroll" href="#testimonials">
+                Testimonials
+               </a>
+            </li>
+                
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
-            </Fade>
-            <h3>I'm a {state} - based <span>{occupation}</span>. {description}.</h3>
-                <hr />
-                <Fade bottom duration={2000}>
+            <h3>I'm a South Carolina based <span>{occupation}</span>. {description}.</h3>
+            <hr />
             <ul className="social">
-               {networks}
-            </ul>
-            </Fade>
+                   {networks}
+              </ul>      
          </div>
       </div>
-      
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
 
-   </header>
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
     );
   }
 }
